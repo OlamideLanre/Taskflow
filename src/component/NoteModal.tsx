3d6d32e8482c8e11e736259d8d4a8584bf4ad96f
OlamideLanre <lanreprincess@gmail.com>
2024-12-31 23:46:34 +0100

    retreiving users input logic

--- src/component/NoteModal.tsx
+++ src/component/NoteModal.tsx
@@ -1,5 +1,63 @@
 import { PlusCircleFilled } from "@ant-design/icons";
+import { useEffect, useState } from "react";
 export const NoteModal = () => {
+  type TASK = {
+    task: string;
+    piority: string;
+    category: string;
+  }[];
+
+  const [Task, setTask] = useState<string>("");
+  const [Piority, setPiority] = useState("");
+  const [Todos, setTodos] = useState<TASK>([]);
+  localStorage.setItem("todolist", JSON.stringify(Todos));
+
+  const curateTodo = () => {
+    let Category = document.getElementById("task-category").value;
+    if (Task.trim() === "") {
+      console.log("task cannot be empty");
+      return;
+    }
+    let updatedTodo = Todos.push({
+      task: Task,
+      piority: Piority,
+      category: Category,
+    });
+    localStorage.setItem("todolist", JSON.stringify(updatedTodo));
+    // console.log(Todos);
+    // console.log(updatedTodo);
+
+    // console.log(Category);
+
+    setTask("");
+  };
+
+  function ckChange(item) {
+    let checkboxs = document.getElementsByName(item.name);
+    let isChecked = document.getElementById(item.id);
+    setPiority(isChecked.value);
+    // console.log(isChecked.value);
+
+    if (isChecked.checked) {
+      for (let i = 0; i < checkboxs.length; i++) {
+        if (!checkboxs[i].checked) {
+          checkboxs[i].disabled = true;
+        } else {
+          checkboxs[i].disabled = false;
+        }
+      }
+    } else {
+      for (let j = 0; j < checkboxs.length; j++) {
+        checkboxs[j].disabled = false;
+      }
+    }
+  }
+
+  useEffect(() => {
+    const persistedTasks = JSON.parse(localStorage.getItem("todolist") || "");
+    setTodos(persistedTasks);
+  }, []);
+
   return (
     <>
       {/* You can open the modal using document.getElementById('ID').showModal() method */}
@@ -24,14 +82,37 @@ export const NoteModal = () => {
             <input
               type="text"
               className="bg-white pr-24 pl-5 py-2 border-black border-b outline-none"
-              placeholder="What your goal today?"
+              placeholder="What's your goal today?"
+              value={Task}
+              onChange={(e) => {
+                setTask(e.target.value);
+              }}
+              required
             />
             <h3 className="font-bold text-lg">Set piority</h3>
             <div className="piority flex items-center gap-2">
-              <input type="checkbox" /> High
+              <input
+                type="checkbox"
+                id="high"
+                name="checkbox"
+                onChange={(e) => {
+                  ckChange(e.target);
+                }}
+                value="High"
+              />
+              High
               <span className="dot high"></span>
               <br />
-              <input type="checkbox" /> Medium
+              <input
+                type="checkbox"
+                id="medium"
+                name="checkbox"
+                onChange={(e) => {
+                  ckChange(e.target);
+                }}
+                value="Medium"
+              />
+              Medium
               <span className="dot medium"></span>
             </div>
 
@@ -62,7 +143,13 @@ export const NoteModal = () => {
                 </button> */}
               </div>
             </div>
-            <button className="bg-black text-lime-100 font-medium py-1 rounded-lg">
+            <button
+              className="bg-black text-lime-100 font-medium py-1 rounded-lg"
+              onClick={(e) => {
+                e.preventDefault();
+                curateTodo();
+              }}
+            >
               Add task
             </button>
           </div>
