da42503a2e0487a7b7c8687b7ad2e6ba384b445a
OlamideLanre <lanreprincess@gmail.com>
2025-01-03 21:29:53 +0100

    displaying todos

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { NoteModal } from "../component/NoteModal";
-import { DeleteFilled } from "@ant-design/icons";
+import { DatabaseFilled, DeleteFilled } from "@ant-design/icons";
+import { ThemeContext } from "../ThemeContext";
 // import { Typography } from "antd";
-export const Task = () => {
+export const Task = ({ tasks, setTasks }) => {
   // const { Paragraph } = Typography;
+  const { Theme } = useContext(ThemeContext);
   const [currentDate, setCurrentDate] = useState(getDate());
   function getDate() {
     const todaysDate = new Date();
-    // const month = todaysDate.getMonth() + 1;
     const month = todaysDate.toLocaleString("dafult", { month: "long" });
     const dateOfWeek = todaysDate.getDate();
     const year = todaysDate.getFullYear();
@@ -16,31 +17,67 @@ export const Task = () => {
   }
   return (
     <>
-      <h2 className="text-black font-semibold text-2xl p-2">{currentDate}</h2>
-      <div className="Container flex border border-black flex-wrap">
-        <div className="note-main flex gap-2 flex-col border border-red-700">
-          <div className="Note-child bg-lime-200 p-4 rounded-md ">
-            <div className="flex justify-between items-center">
-              <div>
-                <p className="text-xl font-light">Bake chrismas cookies</p>
-                <p className="text-xs text-gray-400">{currentDate}</p>
+      <div>
+        <h2 className=" font-semibold text-2xl p-2 text-gray ">
+          {currentDate}
+        </h2>
+        <div className="Container flex flex-wrap">
+          <div className="note-main flex gap-2 flex-col">
+            {tasks.length > 0 ? (
+              tasks.map((t, index: number) => (
+                <div
+                  key={index}
+                  className="Note-child bg-lime-200 p-4 rounded-md "
+                >
+                  <div className="flex justify-between items-center">
+                    <div>
+                      <p className="text-xl font-light text-black">
+                        <span
+                          className={`dot mr-1 ${
+                            t.piority === "High" ? "high" : "medium"
+                          }`}
+                          title={`Piority ${t.piority}`}
+                        ></span>
+                        {t.task}
+                      </p>
+                      <p className="text-xs text-gray-400">
+                        {currentDate}{" "}
+                        <span className="text-xs text-gray-600 font-medium pl-1">
+                          {t.category}
+                        </span>
+                      </p>
+                    </div>
+                    <div>
+                      <input type="checkbox" className="cursor-pointer" />
+                      <DeleteFilled className="cursor-pointer text-black" />
+                    </div>
+                  </div>
+                </div>
+              ))
+            ) : (
+              // {}
+              <div className="text-center text-gray-500 text-3xl ">
+                No plan yet
+                {/* <DatabaseFilled /> */}
               </div>
-              <div>
-                <input type="checkbox" className="cursor-pointer" />
-                <DeleteFilled className="cursor-pointer" />
-              </div>
-            </div>
-          </div>
-          <NoteModal />
-        </div>
+            )}
 
-        {/* <Paragraph editable>hello editable text</Paragraph> */}
-        <div className="Note-child mx-auto">
-          <div className="progress-wrapper text-center">
-            <p className="message">Great start! Keep it going! 🚀</p>
-            <div className="progress-circle">
-              <div className="progress-inner">
-                <p className="percentage">5%</p>
+            <NoteModal tasks={tasks} setTasks={setTasks} />
+          </div>
+
+          {/* <Paragraph editable>hello editable text</Paragraph> */}
+          <div className="Note-child mx-auto">
+            <div className="progress-wrapper text-center">
+              <p className="message">Great start! Keep it going! 🚀</p>
+              <div className="progress-circle">
+                <div
+                  className="progress-inner"
+                  style={{
+                    backgroundColor: Theme == "light" ? "white" : "#111827",
+                  }}
+                >
+                  <p className="percentage">5%</p>
+                </div>
               </div>
             </div>
           </div>
